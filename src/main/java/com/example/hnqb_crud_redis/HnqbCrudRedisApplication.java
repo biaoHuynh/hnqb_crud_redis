a9566eff27585cbbf85fe8b376299015639656e7
biaoHuynh <hnquocbao22@gmail.com>
2021-11-08 10:36:09 +0700

    hnquocbao

---
+++ src/main/java/com/example/hnqb_crud_redis/HnqbCrudRedisApplication.java
@@ -0,0 +1,13 @@
+package com.example.hnqb_crud_redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HnqbCrudRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HnqbCrudRedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hnqb_crud_redis/HnqbCrudRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hnqb_crud_redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HnqbCrudRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
